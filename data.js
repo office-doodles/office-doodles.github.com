56e23f0dddcbf1194292d463a40ac20a4c380f9b
Alexander Artemenko <svetlyak.40wt@gmail.com>
2012-02-09 16:50:08 +0400

    Data moved to separate js file.

---
+++ data.js
@@ -0,0 +1,5 @@
+var images = [
+    {slug: 'cubic-fish', title: 'Cubic Fish'},
+    {slug: 'poly-fish', title: 'Poly Fish'},
+    {slug: 'sad-friday', title: 'Sad Friday'}
+];
--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-$('.images li').each(function() {
-    var $this = $(this);
-    var link = $this.find('a');
-    console.log();
-    var item = '<li><a href="' + link[0].hash + '">' + link.text() + '</a></li>';
+$.each(images, function() {
+    var data = this;
+    var li = $('<li id="' + this.slug + '"><h1><a href="#' + this.slug + '">' + this.title + '</a></h1><img src="images/' + this.slug + '.jpg" alt="' + this.title + '" /></li>');
+    console.log('inserting: ' + data.title);
 
-    $('.navbar ul.nav').append(item);
+    $('.images').append(li);
+
+    li.waypoint(
+        function() {
+            console.log(data.title);
+            $('.nav-title').text(data.title);
+        },
+        {
+            offset: 59
+        }
+    );
 });
 
-$('.navbar').scrollspy()
