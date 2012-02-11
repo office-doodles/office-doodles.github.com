806a314af36ceae12ec234af7863ebebf154a600
Alexander Artemenko <svetlyak.40wt@gmail.com>
2012-02-11 13:45:11 +0400

    Disqus comments.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+---
+---
+var disqus_shortname = 'officedoodles';
+var disqus_developer = 0;
+var disqus_identifier = '';
+var disqus_url = '';
+
+$('.images').append('<div id="disqus_thread"></div>');
+
 if ($('ul.images').length == 1) {
     $.each(images, function() {
         var data = this;
@@ -12,6 +21,37 @@ if ($('ul.images').length == 1) {
         li = $(li);
 
         addthis.button(li.find('a.share')[0], {}, {url: window.location + '#' + this.slug, title: this.title + ' at ' + document.title});
+        // disqus_identifier
+        // disqus_title
+
+        var comments_link = $('<a href="{{ site.production_url }}/#disqus_thread" data-disqus-identifier="post-' + data.slug + '">Comments</a>');
+        console.log('Adding link for ' + data.slug);
+
+        comments_link.click(function(ev) {
+            console.log('Loading comments for ' + data.slug);
+            ev.preventDefault();
+
+            disqus_identifier = 'post-' + data.slug;
+            disqus_url = "{{ site.production_url }}/#!" + data.slug;
+
+            DISQUS.reset({
+              reload: true,
+              config: function () {
+                this.page.identifier = disqus_identifier;
+                this.page.url = disqus_url;
+              }
+            });
+
+            $('#disqus_thread').insertAfter(comments_link).fadeIn();
+        });
+        $(li).append(comments_link);
+
+        // (function() {
+        //     var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        //     dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+        //     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        // })();
+
 
         $('.images').append(li);
 
